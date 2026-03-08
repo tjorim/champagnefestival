8a0b2a644615736434f4690c1a05033d4af63da7
Jorim Tielemans <tielemans.jorim@gmail.com>
2026-03-08 10:37:18 +0100

    fix: make vitest config compatible with vite beta

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,11 @@
 import { resolve } from 'node:path';
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
-import reactPlugin from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [
-    paraglideVitePlugin({
-      project: './project.inlang',
-      outdir: './src/paraglide',
-      strategy: ['localStorage', 'preferredLanguage', 'baseLocale'],
-    }),
-    reactPlugin() as ReturnType<typeof reactPlugin>,
-  ],
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'react',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
